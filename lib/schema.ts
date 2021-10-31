4a0f7f4119698b1669e17170144f03b5d9c91d44
ShadiestGoat <shady@shadygoat.eu>
2021-10-31 22:37:07 +0000

    Fixed undefined

--- lib/schema.ts
+++ lib/schema.ts
@@ -159,53 +159,57 @@ export class Schema<SHLean = unknown> {
             }
         }
         let arrKey = ""
-        function inc(s1:Record<string, unknown> | SchemaTypeOptions<unknown>, s2:Record<string, unknown>):validTot<SHLean> {
-            if (Object.keys(s1 as Record<string, unknown>).includes('type') && Object.keys(s1 as Record<string, unknown>).includes('required')) {
-                const val = testType(s1 as SchemaTypeOptions<unknown>, s2, arrKey)
-                if (val) return val
+        function inc(schm:Record<string, unknown> | SchemaTypeOptions<unknown>, docc:Record<string, unknown>):validTot<SHLean> {
+            if (Object.keys(schm as Record<string, unknown>).includes('type') && Object.keys(schm as Record<string, unknown>).includes('required')) {
+                if (!(typeof docc == "undefined" && !schm.required)) {
+                    const val = testType(schm as SchemaTypeOptions<unknown>, docc, arrKey)
+                    if (val) return val
+                }
             } else {
-                for (const v in s1) {
-                    if (typeof s2 == "object" && !Array.isArray(s2) && !Object.keys(s2).includes(v)) {
-                        if (s1[v].required && !opts.ignoreRequired) return {
+                for (const v in schm) {
+                    if (typeof docc == "object" && !Array.isArray(docc) && !Object.keys(docc).includes(v)) {
+                        if (schm[v].required && !opts.ignoreRequired) return {
                             valid: false,
                             reason: 'required',
                             badKey: v
                         }
-                        else if (s1[v].default && !opts.ignoreDefault) {
-                            if (typeof s1[v].default == "function") (doc as Record<string, unknown>)[v] = s1[v].default()
-                            else (doc as Record<string, unknown>)[v] = s1[v].default
+                        else if (schm[v].default && !opts.ignoreDefault) {
+                            if (typeof schm[v].default == "function") (doc as Record<string, unknown>)[v] = schm[v].default()
+                            else (doc as Record<string, unknown>)[v] = schm[v].default
                         }
-                    } else if (Array.isArray(s1[v].type)) {
-                        if ((s1[v].type as unknown[]).length == 2) {
-                            if (Array.isArray(s2[v])) return {
+                    } else if (Array.isArray(schm[v].type)) {
+                        if ((schm[v].type as unknown[]).length == 2) {
+                            if (Array.isArray(docc[v])) return {
                                 valid: false,
                                 reason: "badType",
                                 badKey: v
                             }
-                            for (const value in (s2[v] as Record<string, never>)) {
-                                const res = inc(s1[v].type[1], (s2[v] as Record<string, never>)[value])
+                            for (const value in (docc[v] as Record<string, never>)) {
+                                const res = inc(schm[v].type[1], (docc[v] as Record<string, never>)[value])
                                 if (!res.valid) return res
                             }
                         }
-                        if (!Array.isArray(s2[v])) return {
+                        if (!Array.isArray(docc[v])) return {
                             valid: false,
                             reason: "badType",
                             badKey: v
                         }
                         arrKey = v
-                        ;(s2[v] as Record<string, unknown>[]).forEach(value => {
-                            const res = inc((s1[v].type as [Record<string, unknown>])[0], value)
+                        ;(docc[v] as Record<string, unknown>[]).forEach(value => {
+                            const res = inc((schm[v].type as [Record<string, unknown>])[0], value)
                             if (!res.valid) return res
                         })
-                    } else if (typeof s1[v].type == "object") {
-                        const res = inc(s1[v].type as Record<string, unknown>, s2[v] as Record<string, unknown>)
+                    } else if (typeof schm[v].type == "object") {
+                        const res = inc(schm[v].type as Record<string, unknown>, docc[v] as Record<string, unknown>)
                         if (!res.valid) return res
                     } else {
-                        const val = testType(s1[v], s2[v], v)
+                        if (typeof docc[v] == "undefined" && !schm[v].required) continue
+                        const val = testType(schm[v], docc[v], v)
                         if (val) return val
                     }
                 }
             }
+
             return { valid: true, res: doc as SHLean }
         }
         return inc(this.schema, doc as Record<string, unknown>)
